cfa091494fac1c4d49f82966de1c982f9165a1b4
MOISES <MOISES@DESKTOP-17FT0L7>
2024-07-11 03:29:01 -0400

    Agregar archivos de proyecto.

---
+++ CapaDatos/CD_conexion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_conexion
+    {
+        private SqlConnection conexion = new SqlConnection("Server=.;DataBase=CRUD grated Security=true");
+        
+        public SqlConnection 
+   
+    
+    }
+}
